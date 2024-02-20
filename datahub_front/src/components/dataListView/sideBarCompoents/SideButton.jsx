b78d50db1fd80c9b828a396c84d48616898a359f
JOODON <launcher37@naver.com>
2024-02-20 14:12:10 +0900

    프론트 부분 카드 디자인완료

--- datahub_front/src/components/dataListView/sideBarCompoents/SideButton.jsx
+++ datahub_front/src/components/dataListView/sideBarCompoents/SideButton.jsx
@@ -3,7 +3,8 @@ export default function SideButton({category}){
     return(
         <div className="button-container" >
             <button style={{display : "flex" , justifyContent : "space-between"}}>
-                {category} <span>▼</span>
+                {category} <span>▼</span> {/* 이 부분을 기깔난 아이콘 으로 바꿔 주세요*/}
+                            
             </button>
         </div>
     )
