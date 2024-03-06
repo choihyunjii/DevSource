70c620281a3de88d6f8c8d44609d78630120553b
JOODON <launcher37@naver.com>
2024-03-06 12:37:46 +0900

    정규화 작업 시작

--- datahub_front/src/App.js
+++ datahub_front/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import TemporaryAppRouter from "./routes/TemporaryAppRouter";
 import Header from "./Layout/Header/Header";
 import TablePage from "./devSource/Components/page/TablePage";
+import Normalization from "./normalization/page/Normalization";
 
 function App() {
   return (
       <div>
           <Header/>
-          <TemporaryAppRouter/>
-          {/*<TablePage/>*/}
+          {/*<TemporaryAppRouter/>*/}
+          <Normalization/>
       </div>
 
   );
