86535b487c4b26dbdb1c541d830faa751eb2fe95
choihyunjii <such1123@naver.com>
2024-03-11 02:07:45 +0900

    project 협업자 찐 끝

--- datahub_front/src/project/components/uI/SmallTitleUI.jsx
+++ datahub_front/src/project/components/uI/SmallTitleUI.jsx
@@ -1,7 +1,7 @@
 export default function SmallTitleUI({title,className}){
     return(
-        <div style={{display:"flex", justifyContent:"center" ,gap:"100px",marginLeft:"35%", width:"68%"}}>
-            <h4 className={className} >{title}</h4>
+        <div style={{display:"flex", justifyContent:"center" ,gap:"90px", width:"70%",marginLeft:"120px"}}>
+            <h4 className={className}>{title}</h4>
             <div style={{padding:"10px"}}>2명</div>
         </div>
     )
